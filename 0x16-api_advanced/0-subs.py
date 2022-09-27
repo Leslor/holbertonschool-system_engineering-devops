d27a683d491a313f02ab36f3f9aa025c98fb394c
LesLor <leslie.paz.am@gmail.com>
2022-09-27 17:57:29 +0000

    Feat: 0x16. API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API and
+    returns the number of subscribers (not active
+    users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            'User-Agent': "linux:Holberton:v1.0.0 (by /u/leslor)"
+                }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    """function that queries the Reddit API and
+    prints the titles of the first 10 hot posts
+    listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+            'User-Agent': "linux:Holberton:v1.0.0 (by /u/leslor)"
+                }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        response = response.json().get('data')
+        for i in response.get('children'):
+            print(i.get('data').get('title'))
+    else:
+        print(None)
+        return
