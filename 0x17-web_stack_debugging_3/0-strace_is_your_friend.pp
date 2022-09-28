bd3881ac568622cf58f93210c5e1665869cf9baa
LesLor <leslie.paz.am@gmail.com>
2022-09-28 17:41:36 +0000

    Feat: task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Debugg 500 Error
-exec { 'error apache2 500'
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+exec { 'error apache2 500':
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
