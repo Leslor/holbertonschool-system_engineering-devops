eb16fdc292a7d0a93a6087a319752f9180f2c6cc
leslor <leslie.paz.am@gmail.com>
2022-09-28 12:33:43 -0500

    adding the path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Debugg 500 Error
 exec { 'error apache2 500'
-  command => ['sed -i "s/phpp/php/g" /var/www/html/wp-settings.php']
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
