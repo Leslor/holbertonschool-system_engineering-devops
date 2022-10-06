bf0bba02fc4f1dafa5f9ae4d4f84b7d8414bc8ef
LesLor <leslie.paz.am@gmail.com>
2022-10-06 17:11:09 +0000

    Feat: 0x1B. Web stack debugging #4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,6 @@
 exec { 'update the limit':
-  command => 'sed -i "s/15/3000/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-exec { 'update nginx':
-  command => 'service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command  => 'sed -i "s/15/4096/" /etc/default/nginx; service nginx restart',
+  user     => 'root',
+  provider => 'shell',
+  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
